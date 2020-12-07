5cc637d95c2bb209f5a9556cd5f1a8f5da80d3d8
Paje <sryakuza@gmail.com>
2020-12-07 03:59:19 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega 1 usuario
+//$root = new Usuario();
+//$root->loadById(4);
+//echo $root;
 
-$root->loadById(4);
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode(($lista));
 
-echo $root;
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("e");
+//echo json_encode($search);
+
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("paje", "321654");
+echo $usuario;
 
  ?>
